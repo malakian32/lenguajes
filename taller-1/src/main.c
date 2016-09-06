755e664f9af6155dbc15880a96d882d6db0ed40b
Víctor Rodriguez <vrodmz@gmail.com>
2016-09-06 01:31:06 -0500

    File I/O supported now. All changes will be saved in ouput.txt and recovered from there

--- taller-1/src/main.c
+++ taller-1/src/main.c
@@ -4,17 +4,21 @@
 int PHENOTYPIC_MODE = 0;
 int GENOTYPIC_MODE = 1;
 int MODE = 0;
+int counter = 0;
 GtkWidget *editorModeButton;
 GtkWidget *editorTextView;
 GtkWidget *lexicalAnalysisTextView;
+GtkWidget *meaningsTextView;
+GtkWidget *statsTextView;
 GtkTextBuffer *editorTextBuffer;
 GtkTextBuffer *lexicalAnalysisTextBuffer;
+GtkTextBuffer *meaningsTextBuffer;
+GtkTextBuffer *statsTextBuffer;
+FILE *file;
 
 int main(int argc, char *argv[]) {
 	GtkBuilder *builder;
 	GtkWidget *window;
-
-	initStats();
 
 	gtk_init(&argc, &argv);
 
@@ -24,12 +28,22 @@ int main(int argc, char *argv[]) {
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "main-window"));
 	gtk_builder_connect_signals(builder, NULL);
 
-	editorModeButton = GTK_WIDGET(
-			gtk_builder_get_object(builder, "editorMode"));
+	editorModeButton = GTK_WIDGET(gtk_builder_get_object(builder, "editorMode"));
+
 	editorTextView = GTK_WIDGET(gtk_builder_get_object(builder, "editor"));
-	lexicalAnalysisTextView = GTK_WIDGET(gtk_builder_get_object(builder, "lexicalAnalysis"));
 	editorTextBuffer = gtk_text_view_get_buffer(editorTextView);
+
+	lexicalAnalysisTextView = GTK_WIDGET(gtk_builder_get_object(builder, "lexicalAnalysis"));
 	lexicalAnalysisTextBuffer = gtk_text_view_get_buffer(lexicalAnalysisTextView);
+
+	meaningsTextView = GTK_WIDGET(gtk_builder_get_object(builder, "meanings"));
+	meaningsTextBuffer = gtk_text_view_get_buffer(meaningsTextView);
+
+	statsTextView = GTK_WIDGET(gtk_builder_get_object(builder, "stats"));
+	statsTextBuffer = gtk_text_view_get_buffer(statsTextView);
+
+	initStats();
+	readFile();
 
 	g_object_unref(builder);
 	gtk_widget_show(window);
@@ -57,25 +71,67 @@ void keyPressedInEditor() {
 	char *text;
 	int size;
 
-	if (MODE == GENOTYPIC_MODE) {
-		size = gtk_text_buffer_get_char_count(editorTextBuffer);
-		gtk_text_buffer_get_start_iter(editorTextBuffer, &start);
-		gtk_text_buffer_get_end_iter(editorTextBuffer, &end);
-		text = gtk_text_buffer_get_text(editorTextBuffer, &start, &end, FALSE);
+	size = gtk_text_buffer_get_char_count(editorTextBuffer);
+	gtk_text_buffer_get_start_iter(editorTextBuffer, &start);
+	gtk_text_buffer_get_end_iter(editorTextBuffer, &end);
+	text = gtk_text_buffer_get_text(editorTextBuffer, &start, &end, FALSE);
 
+	if (MODE == GENOTYPIC_MODE) {
 		updateLexicalAnalysis(text, size);
 		updateMeanings(text);
 		updateStats(text);
 	}
+
+	if (counter % 3 == 0) {
+		updateFile(text);
+	}
+
+	counter++;
+}
+
+void updateFile(char *text) {
+	file = fopen("output.txt", "w");
+
+	if (file == NULL) {
+		printf("Error opening file!\n");
+		exit(1);
+	}
+
+	fprintf(file, "%s", text);
+	fclose(file);
+}
+
+void readFile() {
+	char buffer[255];
+	char *text = malloc(100000000);
+	file = fopen("output.txt", "r");
+
+	if (file == NULL) {
+		printf("Error opening file!\n");
+		exit(1);
+	}
+
+	while (fgets(buffer, 255, file)) {
+		strcat(text, buffer);
+	}
+
+	updateTextEditor(text, strlen(text));
+
+	fclose(file);
+}
+
+void updateTextEditor(char* text, int size) {
+	gtk_text_buffer_set_text(editorTextBuffer, text, size);
 }
 
 void updateLexicalAnalysis(char* text, int size) {
 	gtk_text_buffer_set_text(lexicalAnalysisTextBuffer, text, size);
 }
 
-void updateMeanings(char* text) {
+void updateMeanings(char* text, int size) {
+
 }
 
-void updateStats(char* text) {
+void updateStats(char* text, int size) {
 
 }
